7a2e388ae00345f4e8da730180618e1d54132208
leeenx <lee200822@126.com>
2017-07-17 11:27:38 +0800

    add a request function

--- mobile-swiper.js
+++ mobile-swiper.js
@@ -14,46 +14,46 @@ var mobileSwiper = function(selector) {
 			console.log("向右"); 
 			this.queue.unshift(this.queue.pop()); 
 			lock = 1; 
-			swap();
+			swap("right");
 		} else if(offsetX >= 50) {
 			// 向左
 			console.log("向左");
 			this.queue.push(this.queue.shift()); 
 			lock = 1; 
-			swap();
+			swap("left");
 		} 
 	}.bind(this), 
-	swap = function(withoutTransition) { 
+	swap = function(orientation) { 
 		var queue = [].concat(this.queue), 
-			count = 0, 
-			len = this.visual.length, 
-			visual = new Array(len), 
+			total = this.virtual.length, // item 总数
+			last = total - 1, // 最后一个索引
+			collect = 0, // 提取数
+			virtual = new Array(total), 
 			odd = 1; 
 		// 提取前三个元素与后三个元素
-		while(count<5 && queue.length>0) { 
-			visual[odd ? queue.shift() : queue.pop()] = css[count++] + (withoutTransition ? "" : transition); 
+		while(collect<5 && queue.length>0) { 
+			virtual[odd ? queue.shift() : queue.pop()] = css[collect == last && !odd && "right" == orientation ? ++collect : collect++]; // 做一个方向优化
 			odd = !odd; // 取反
-		}
+		} 
 		// 对比一下数组 
-		for(var i=0; i<len; ++i) { 
-			visual[i] != this.visual[i] && (this.visual[i] = visual[i], this.item[i].style.cssText = this.visual[i] || "visibility: hidden"); 
+		for(var i=0; i<total; ++i) { 
+			virtual[i] != this.virtual[i] && (this.virtual[i] = virtual[i], this.item[i].style.cssText = this.virtual[i] || "visibility: hidden"); 
 		}
 	}.bind(this), 
 	rem = function(px) {
 		return px / 40 + "rem"; 
 	}, 
-	transition = "-webkit-transition: -webkit-transform .3s ease", 
 	css = [
-		"z-index: 3; -webkit-transform: translate3d(0, 0, 10px) scale3d(1, 1, 1); visibility: visible;", 
-		"z-index: 2; -webkit-transform: translate3d(" + rem(-148) + ", 0, 6px) scale3d(.8, .8, 1); visibility: visible;", 
-		"z-index: 2; -webkit-transform: translate3d(" + rem(148) + ", 0, 6px) scale3d(.8, .8, 1); visibility: visible;", 
-		"z-index: 1; -webkit-transform: translate3d(" + rem(-240) + ", 0, 2px) scale3d(.667, .667, 1); visibility: visible;", 
-		"z-index: 1; -webkit-transform: translate3d(" + rem(240) + ", 0, 2px) scale3d(.667, .667, 1); visibility: visible;"
+		"-webkit-transition: -webkit-transform .3s ease; z-index: 3; -webkit-transform: translate3d(0, 0, 10px) scale3d(1, 1, 1); visibility: visible;", 
+		"-webkit-transition: -webkit-transform .3s ease; z-index: 2; -webkit-transform: translate3d(" + rem(-148) + ", 0, 6px) scale3d(.8, .8, 1); visibility: visible;", 
+		"-webkit-transition: -webkit-transform .3s ease; z-index: 2; -webkit-transform: translate3d(" + rem(148) + ", 0, 6px) scale3d(.8, .8, 1); visibility: visible;", 
+		"-webkit-transition: -webkit-transform .3s ease; z-index: 1; -webkit-transform: translate3d(" + rem(-240) + ", 0, 2px) scale3d(.667, .667, 1); visibility: visible;", 
+		"-webkit-transition: -webkit-transform .3s ease; z-index: 1; -webkit-transform: translate3d(" + rem(240) + ", 0, 2px) scale3d(.667, .667, 1); visibility: visible;"
 	]; 
 
 	this.init = function(list) {
 		this.container = list; 
-		list.style["-webkit-transform-style"] = "preserve-3d"; 
+		this.container.style["-webkit-transform-style"] = "preserve-3d"; 
 		this.item = list.querySelectorAll("li"); 
 		for(var i=0; i<this.item.length; ++i) {this.item[i].style.visibility = "hidden"}
 		this.queue = function(len) { // 索引列表，用于处理切换的序号队列 
@@ -62,8 +62,8 @@ var mobileSwiper = function(selector) {
 			for(var i=0; i<len; ++i) arr[i] = i; 
 			return arr; 
 		}(this.item.length); 
-		this.visual = new Array(this.item.length); // 与 item 做对应的虚拟DOM
-		swap("without transition"); // 初始排版
+		this.virtual = new Array(this.item.length); // 与 item 做对应的虚拟DOM
+		swap(); // 初始排版
 		if(this.item.length <= 1) return ;
 		this.container.addEventListener("touchstart", touchstartHandle); 
 		this.container.addEventListener("touchmove", touchmoveHandle);  
